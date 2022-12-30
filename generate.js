5d10e0cf2471acc29b488acc96b6638eb7d99b55
Denis Perepelitsyn <fropsas@gmail.com>
2022-12-30 17:29:34 +0800

    fix: new nfts

--- generate.js
+++ generate.js
@@ -4,22 +4,9 @@ const countNft = 5000;
 
 const standardMeta = {
     "name": "Cryptus x Crypton 2023 #0",
-    "image": "https://raw.githubusercontent.com/cdevs-group/cryptus-nft-metadata/test/0/0.png",
+    "image": "https://raw.githubusercontent.com/cdevs-group/cryptus-nft-metadata/master/0/0.png",
     "used": false,
-    "attributes": [
-        {
-            "trait_type": "Gift1",
-            "value": "iPhone 14 PRO"
-        },
-        {
-            "trait_type": "Gift2",
-            "value": "Скидка 10% на мерч Cryptus"
-        },
-        {
-            "trait_type": "Gift3",
-            "value": "Бесплатный доступ в клуб Cryptus"
-        }
-    ]
+    "attributes": []
 }
 
 const bootstrap = () => {
@@ -27,7 +14,7 @@ const bootstrap = () => {
         fs.mkdirSync(`./${i.toString()}`, { recursive: true })
         const metaNft = standardMeta;
         metaNft.name = `Cryptus x Crypton 2023 #${i}`;
-        metaNft.image = `https://raw.githubusercontent.com/cdevs-group/cryptus-nft-metadata/test/${i}/${i}.png`
+        metaNft.image = `https://raw.githubusercontent.com/cdevs-group/cryptus-nft-metadata/master/${i}/${i}.png`
         fs.writeFileSync(`./${i.toString()}/${i}.json`, JSON.stringify(metaNft))
         fs.copyFileSync('./standardNftImg.png', `./${i.toString()}/${i}.png`);
     }
