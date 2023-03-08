fca289c4ff85c5a88daf24b62196812b9bc2df57
Denis <fropsas@gmail.com>
2023-03-08 20:26:47 +0200

    chore: everything is activated except prime and impossible

---
+++ activate.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+const countNft = 5000;
+
+const bootstrap = async () => {
+    for (let i = 0; i < countNft; i++) {
+        const meta = JSON.parse(fs.readFileSync(`./${i.toString()}/${i}.json`, 'utf8'));
+        if (meta.attributes.length === 0) continue;
+        if (meta.used) continue;
+        if (meta.attributes[0]['value'].includes('Prime')) continue;
+        if (meta.attributes[0]['value'].includes('Impossible')) continue;
+
+        meta.used = true;
+        meta.image = meta.image.replace('.png', '_used.png');
+        fs.writeFileSync(`./${i.toString()}/${i}.json`, JSON.stringify(meta));
+    }
+}
+
+bootstrap()
